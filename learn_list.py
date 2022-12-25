1410054d7cbd78afccc2ac9daed4899a16d935ed
xuheyou <2578449796@qq.com>
2022-12-25 20:02:26 +0800

    添加3个文件

---
+++ learn_list.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 19:57:41 2022
+
+@author: xhy
+"""
+
---
+++ learn_tuple.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 20:00:09 2022
+
+@author: xhy
+"""
+
